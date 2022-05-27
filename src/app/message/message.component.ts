2394622c9e9311eadb5763c272c29f96c4948615
dwagner <duane.wagner@infor.com>
2022-05-27 10:11:43 -0500

    Modified ionic list starter to demonstrate the textarea scroll bug that only happens on iOS devices.

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ViewChild} from '@angular/core';
 import { Message } from '../services/data.service';
+import {DomController, IonTextarea} from "@ionic/angular";
+import {timer} from "rxjs";
 
 @Component({
   selector: 'app-message',
@@ -7,14 +9,34 @@ import { Message } from '../services/data.service';
   styleUrls: ['./message.component.scss'],
 })
 export class MessageComponent implements OnInit {
+  @ViewChild('ita') ionTextarea!: IonTextarea;
   @Input() message: Message;
+  editing = false;
 
-  constructor() { }
+  constructor(private domCtrl: DomController) { }
 
-  ngOnInit() {}
-
-  isIos() {
-    const win = window as any;
-    return win && win.Ionic && win.Ionic.mode === 'ios';
+  ngOnInit() {
   }
+
+  onEdit() {
+    this.editing = !this.editing;
+    if (this.editing) {
+      // wait until ngIf has put textarea in dom
+      this.domCtrl.read(() => {
+        try {
+          // setFocus was successfully called, but didn't work without a delay
+          timer(50).subscribe(()=>{
+            this.ionTextarea.setFocus().then(()=> {
+              console.log("focused");
+              // textarea is focused on iPhone, but it did not scroll out from
+              // under the on-screen keyboard.
+            });
+          });
+        } catch (e) {
+          console.error(JSON.stringify(e,null,2));
+        }
+      });
+    }
+  }
+
 }
